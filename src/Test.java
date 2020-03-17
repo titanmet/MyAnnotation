33e6d39fea7e07ffc78decbbeb3a1442b3eec3e2
titanmet <titanmet@mail.ru>
2020-03-17 21:57:07 +0300

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+@MyAnnotation
+public class Test {
+    @MyAnnotation
+    private String name;
+
+    @MyAnnotation
+    public Test() {
+
+    }
+
+    @MyAnnotation
+    public void test(@MyAnnotation int value) {
+        @MyAnnotation String localVar = "Hello";
+    }
+
+    @MyAnnotation
+    public static void main(@MyAnnotation String[] args) {
+        
+    }
+}
